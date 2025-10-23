d14dd6d621ff7e918c260a9ced74166988f4f29e
MelkerI03 <Melker.Isaksson@outlook.com>
2025-10-23 15:30:45 +0200

    🔧 config: ssh auth

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,9 +1,20 @@
 { ... }:
 {
+  services.ssh-agent.enable = true;
+
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
 
-    matchBlocks."*" = { };
+    matchBlocks = {
+      "github.com" = {
+        user = "git";
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "*" = {
+        addKeysToAgent = "yes";
+      };
+    };
   };
 }
