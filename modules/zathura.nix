24273f15c66e2aefc0810ab63e26758dfdfcdbfb
MelkerI03 <Melker.Isaksson@outlook.com>
2026-04-07 09:10:51 +0200

    🚀 perf: pdf-viewer improvements

--- modules/zathura.nix
+++ modules/zathura.nix
@@ -1,11 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zathura = {
     enable = true;
 
+    package = pkgs.zathura.override {
+      useMupdf = true;
+    };
+
     options = {
       synctex = true;
       guioptions = "sv"; # show: Statusbar, vertical scroll
+
+      selection-clipboard = "clipboard";
+
+      page-cache-size = 1000;
+      render-all = true;
+      incremental-search = true;
     };
   };
 }
